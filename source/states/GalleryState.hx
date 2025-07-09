0462931139ae88fc7d06dda527b6f75816db063d
MrMadera <ismaelglhs@gmail.com>
2025-07-09 15:35:16 +0100

    improvements and some tweaks!

--- source/states/GalleryState.hx
+++ source/states/GalleryState.hx
@@ -1,6 +1,7 @@
 package states;
 
 import flixel.addons.display.FlxBackdrop;
+import shaders.WiggleEffect;
 
 class GalleryState extends MusicBeatState
 {
@@ -13,16 +14,23 @@ class GalleryState extends MusicBeatState
     var leftArrow:FlxSprite;
     var rightArrow:FlxSprite;
 
-    var optGrp:FlxTypedGroup<FlxSprite>;
+    var optGrp:FlxTypedGroup<GalleryObject>;
     var optArray:Array<String> = [
+        'outdated_concepts',
         'outdated_concepts'
     ];
+
+	var wiggle:WiggleEffect = null;
+
+    private static var curSelected:Int = 0;
 
     override function create() 
     {
         super.create();
 
         FlxG.mouse.visible = true;
+
+		wiggle = new WiggleEffect(2, 4, 0.002, WiggleEffectType.DREAMY);
         
         bg = new FlxSprite();
         bg.loadGraphic(Paths.image('gallery/bg'));
@@ -49,41 +57,107 @@ class GalleryState extends MusicBeatState
         bfIconsBottom.antialiasing = ClientPrefs.data.antialiasing;
         add(bfIconsBottom);
 
-        leftArrow = new FlxSprite(15, 0);
+        optGrp = new FlxTypedGroup<GalleryObject>();
+        add(optGrp);
+
+        leftArrow = new FlxSprite(45, 0);
         leftArrow.loadGraphic(Paths.image('gallery/arrow'));
         leftArrow.screenCenter(Y);
         leftArrow.antialiasing = ClientPrefs.data.antialiasing;
         add(leftArrow);
 
-        rightArrow = new FlxSprite(30, 0);
+        leftArrow.y -= 10;
+        FlxTween.tween(leftArrow, {y: leftArrow.y + 20}, 7, {ease: FlxEase.quartInOut, type: PINGPONG});
+
+        rightArrow = new FlxSprite(45, 0);
         rightArrow.loadGraphic(Paths.image('gallery/arrow'));
         rightArrow.screenCenter(Y);
         rightArrow.antialiasing = ClientPrefs.data.antialiasing;
-        rightArrow.x = FlxG.width - rightArrow.width - 30;
+        rightArrow.x = FlxG.width - rightArrow.width - 45;
         rightArrow.flipX = true;
         add(rightArrow);
 
-        optGrp = new FlxTypedGroup<FlxSprite>();
-        add(optGrp);
+        rightArrow.y -= 10;
+        FlxTween.tween(rightArrow, {y: rightArrow.y + 20}, 7, {ease: FlxEase.quartInOut, type: PINGPONG});
 
         for(i in 0...optArray.length)
         {
-            var spr = new FlxSprite();
+            var spr = new GalleryObject();
             spr.loadGraphic(Paths.image('gallery/' + optArray[i]));
             spr.antialiasing = ClientPrefs.data.antialiasing;
             spr.screenCenter();
+            spr.targetX = i;
             spr.x += FlxG.width * i;
+            spr.shader = wiggle;
             optGrp.add(spr);
+
+            spr.y -= 10;
+            FlxTween.tween(spr, {y: spr.y + 20}, 7, {ease: FlxEase.quartInOut, type: PINGPONG});
         }
+
+        changeSelect();
     }
 
     override function update(elapsed:Float)
     {
         super.update(elapsed);
 
+		if (wiggle != null) {
+			wiggle.update(elapsed);
+		}
+
         if(controls.BACK)
         {
             MusicBeatState.switchState(new MainMenuState());
         }
+
+        var leftMult:Float = FlxMath.lerp(leftArrow.scale.x, 1, elapsed * 9);
+        leftArrow.scale.set(leftMult, leftMult);
+
+        var rightMult:Float = FlxMath.lerp(rightArrow.scale.x, 1, elapsed * 9);
+        rightArrow.scale.set(rightMult, rightMult);
+
+        if(controls.UI_RIGHT_P)
+        {
+            changeSelect(1);
+            rightArrow.scale.set(1.25, 1.075);
+        }
+        else if(controls.UI_LEFT_P)
+        {
+            changeSelect(-1);
+            leftArrow.scale.set(1.25, 1.075);
+        }
+    }
+
+    function changeSelect(change:Int = 0)
+    {
+        curSelected = FlxMath.wrap(curSelected + change, 0, optArray.length - 1);
+
+		for (num => item in optGrp.members)
+		{
+			item.targetX = num - curSelected;
+			item.alpha = 0.6;
+			if (item.targetX == 0) item.alpha = 1;
+		}
+    }
+}
+
+class GalleryObject extends FlxSprite
+{
+    public var targetX:Float = 0;
+    public var distancePerItem:FlxPoint = new FlxPoint(1280, 0);
+    public var startPosition:FlxPoint = new FlxPoint(0, 0);
+    
+    public function new(x:Float = 0, y:Float = 0, ?graphic:Dynamic = null)
+    {
+        super(x, y, graphic);
+    }
+
+    override function update(elapsed:Float)
+    {
+        super.update(elapsed);
+
+		var lerpVal:Float = Math.exp(-elapsed * 9.6);
+		x = FlxMath.lerp((targetX * distancePerItem.x) + startPosition.x, x, lerpVal);
     }
 }
