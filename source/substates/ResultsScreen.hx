8643c47f9a01a06db69526aa7d7825134f053764
MrMadera <ismaelglhs@gmail.com>
2025-09-20 12:20:19 +0100

    more progress n stuff + some fixes :D

--- source/substates/ResultsScreen.hx
+++ source/substates/ResultsScreen.hx
@@ -7,6 +7,10 @@ import states.FreeplayState;
 
 class ResultsScreen extends MusicBeatSubstate
 {
+    var stripeSpeed:Int = 25;
+    var iconSpeed:Int = 15;
+    var patternSpeed:Int = 15;
+
     override function create() 
     {
         super.create();
@@ -15,33 +19,41 @@ class ResultsScreen extends MusicBeatSubstate
         bg.makeGraphic(FlxG.width, FlxG.height, 0xFFCFC6F3);
         add(bg);
 
-        var bgStripe = new FlxSprite();
-        bgStripe.loadGraphic(Paths.image('resultsScreen/newResultsScreen/stripe'));
+        var lines:FlxBackdrop = new FlxBackdrop(Paths.image('gallery/lines'), #if (flixel <= "5.0.0") 0.2, 0.2, true, true #else XY #end);
+        lines.velocity.set(75, 75);
+        lines.alpha = 0.45;
+        lines.antialiasing = ClientPrefs.data.antialiasing;
+        add(lines);
+
+        var bgStripe = new FlxBackdrop(Paths.image('resultsScreen/newResultsScreen/stripe'), #if (flixel <= "5.0.0") 0.2, 0.2, true, true #else X #end);
         bgStripe.antialiasing = ClientPrefs.data.antialiasing;
+        bgStripe.velocity.set(stripeSpeed, 0);
         bgStripe.blend = ADD;
         add(bgStripe);
 
         var bfIconLeft = new FlxBackdrop(Paths.image('resultsScreen/newResultsScreen/icon'), #if (flixel <= "5.0.0") 0.2, 0.2, true, true #else Y #end);
-        bfIconLeft.velocity.set(0, -10);
+        bfIconLeft.velocity.set(0, -iconSpeed);
         bfIconLeft.antialiasing = ClientPrefs.data.antialiasing;
         add(bfIconLeft);
         
         var bfIconRight = new FlxBackdrop(Paths.image('resultsScreen/newResultsScreen/icon'), #if (flixel <= "5.0.0") 0.2, 0.2, true, true #else Y #end);
         bfIconRight.x = FlxG.width - bfIconRight.width;
-        bfIconRight.velocity.set(0, 10);
+        bfIconRight.velocity.set(0, iconSpeed);
         bfIconRight.antialiasing = ClientPrefs.data.antialiasing;
         add(bfIconRight);
 
         var patternDown = new ResultsScreenPattern(0, 0);
         patternDown.y = FlxG.height - patternDown.height;
-        patternDown.velocity.set(10, 0);
+        patternDown.darkPattern.velocity.set(patternSpeed, 0);
+        patternDown.lightPattern.velocity.set(-patternSpeed, 0);
         patternDown.antialiasing = ClientPrefs.data.antialiasing;
         add(patternDown);
 
         var patternUp = new ResultsScreenPattern(0, 0, true);
         patternUp.y = 0;
         patternUp.flipY = true;
-        patternUp.velocity.set(-10, 0);
+        patternUp.darkPattern.velocity.set(-patternSpeed, 0);
+        patternUp.lightPattern.velocity.set(patternSpeed, 0);
         patternUp.antialiasing = ClientPrefs.data.antialiasing;
         add(patternUp);
 
@@ -80,8 +92,8 @@ class ResultsScreen extends MusicBeatSubstate
  */
 class ResultsScreenPattern extends FlxSpriteGroup
 {
-    var darkPattern:FlxBackdrop;
-    var lightPattern:FlxBackdrop;
+    public var darkPattern:FlxBackdrop;
+    public var lightPattern:FlxBackdrop;
 
     public function new(x:Float, y:Float, flipPatternY:Bool = false)
     {
