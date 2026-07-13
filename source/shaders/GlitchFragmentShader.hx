342c4cf1db34f7432f15ff1cbc18cc4aa5432e13
Fioon <135500399+Fioon@users.noreply.github.com>
2026-07-13 21:55:26 +0800

    try

--- source/shaders/GlitchFragmentShader.hx
+++ source/shaders/GlitchFragmentShader.hx
@@ -7,9 +7,7 @@ class GlitchFragmentShader extends FlxShader
 	@glFragmentSource('
 
 	#pragma header
-	vec2 uv = openfl_TextureCoordv.xy;
-	vec2 fragCoord = openfl_TextureCoordv*openfl_TextureSize;
-	vec2 iResolution = openfl_TextureSize;
+	
 	uniform float iTime;
 	#define iChannel0 bitmap
 	#define texture flixel_texture2D
@@ -70,6 +68,10 @@ class GlitchFragmentShader extends FlxShader
 	
 	void mainImage()
 	{
+        //vec2 uv = openfl_TextureCoordv.xy;
+	
+        vec2 fragCoord = openfl_TextureCoordv*openfl_TextureSize;
+	    vec2 iResolution = openfl_TextureSize;
 		vec2 uv = fragCoord/iResolution.xy;
 		
 		float gps = 15.;// glitch per seconds
