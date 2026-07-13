679c9c096083a83b7db0325d452b50d073016f69
Fioon <135500399+Fioon@users.noreply.github.com>
2026-07-13 18:54:52 +0800

    Update StoryMenuState.hx

--- source/states/StoryMenuState.hx
+++ source/states/StoryMenuState.hx
@@ -275,6 +275,7 @@ class StoryMenuState extends MusicBeatState
 		changeWeek();
 		changeDifficulty();
 
+		addTouchPad('LEFT_FULL', 'A_B_X_Y');
 		super.create();
 	}
 
@@ -282,6 +283,8 @@ class StoryMenuState extends MusicBeatState
 		persistentUpdate = true;
 		changeWeek();
 		super.closeSubState();
+		removeTouchPad();
+		addTouchPad('LEFT_FULL', 'A_B_X_Y');
 	}
 
 	override function update(elapsed:Float)
@@ -395,14 +398,16 @@ class StoryMenuState extends MusicBeatState
 			else if (changeDiff)
 				changeDifficulty();
 
-			if(FlxG.keys.justPressed.CONTROL)
+			if(FlxG.keys.justPressed.CONTROL || touchPad.buttonX.justPressed)
 			{
 				persistentUpdate = false;
+				removeTouchPad();
 				openSubState(new GameplayChangersSubstate());
 			}
-			else if(controls.RESET)
+			else if(controls.RESET || touchPad.buttonY.justPressed)
 			{
 				persistentUpdate = false;
+				removeTouchPad();
 				openSubState(new ResetScoreSubState('', curDifficulty, '', curWeek));
 				//FlxG.sound.play(Paths.sound('scrollMenu'));
 			}
